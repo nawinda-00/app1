fc0d50af410fad1948ed364528a45eb234d70a82
nawinda <nawinda.99dl@gmail.com>
2026-08-03 10:59:54 +0700

    EventData2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
+import { EventData2 } from './event-data';
 
-import { Calculator2 } from './calculator';
-
-function App() {
-  return <Calculator2/> 
+export default function App() {
+  return <EventData2/> 
 }
-
-export default App;
